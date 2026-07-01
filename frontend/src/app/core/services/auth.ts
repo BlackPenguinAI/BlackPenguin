c38e5bb82564b6927511a81e9b213d11585a71d7
Jorge Jesús <jorgektch@gmail.com>
2026-07-01 15:02:36 -0500

    Deploy

--- frontend/src/app/core/services/auth.ts
+++ frontend/src/app/core/services/auth.ts
@@ -6,7 +6,8 @@ import { Observable, tap, catchError, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8000/api/v1'; 
+  #private apiUrl = 'http://localhost:8000/api/v1';
+  
 
   constructor(private http: HttpClient) {}
 
