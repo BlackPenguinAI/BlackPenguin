b32a074cb50cd2c30aae2a3dbe39bc30a0f7ef53
Jorge Jesús <jorgektch@gmail.com>
2026-07-09 13:55:09 -0500

    Fix: init db

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -2,6 +2,9 @@
 from app.db.postgres import Base  # Importa tu Base original aquí
 
 # Importa todos tus modelos debajo
-from app.modules.auth.models import User, UserRole
-from app.modules.companies.models import Company
-from app.modules.sales.models import WaitlistEmail, Lead
+from app.modules.ai.models import *
+from app.modules.auth.models import *
+from app.modules.integrations.models import *
+from app.modules.properties.models import *
+from app.modules.sales.models import *
+from app.modules.tenants.models import *
